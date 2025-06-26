e767c2729c00be413f59c54f9691406c0808497b
shilpigangwal <99666287+shilpigangwal@users.noreply.github.com>
2025-06-26 19:42:55 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -32,7 +32,7 @@ conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
 
 # Create user interface
 st.title("🗣️ Conversational Chatbot")
-st.subheader("㈻ Simple Chat Interface for LLMs by Build Fast with AI")
+st.subheader("㈻ Simple Chat Interface for LLMs")
 
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
